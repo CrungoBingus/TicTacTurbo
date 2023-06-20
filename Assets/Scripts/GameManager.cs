1fb331f02189471bdbc83e82b99617fa72bb1a5b
Clark P <56273765+CrungoBingus@users.noreply.github.com>
2023-06-20 19:54:19 -0400

    Quick change! forgor to thing?!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -145,6 +145,7 @@ public class GameManager : MonoBehaviour
         if (playersTurnCount == 3)
             playersTurn = PlayerColors.Yellow;
 
+        currentEffect = m_effectBase;
         DisplayManager.Instance.DisplayNewEffect(m_effectBase);
     }
 
